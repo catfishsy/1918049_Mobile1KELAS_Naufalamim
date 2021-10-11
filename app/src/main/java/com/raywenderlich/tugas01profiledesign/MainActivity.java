804ae4162dbbf7ee7f06d02b756417172e5fa4d1
amima <ninja_cat712@yahoo.com>
2021-10-11 14:23:50 +0700

    Awwal profile UI 1918049

---
+++ app/src/main/java/com/raywenderlich/tugas01profiledesign/MainActivity.java
@@ -0,0 +1,13 @@
+package com.raywenderlich.tugas01profiledesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
